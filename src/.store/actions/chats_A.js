2119fce281f47c4e179f15f0ce0fce2df6dc8baf
G-Prokhorov <grisha.prokhorov03@gmail.com>
2021-07-02 18:08:15 +0300

    create get chats reducer

---
+++ src/.store/actions/chats_A.js
@@ -0,0 +1,21 @@
+import {SET_CHATS} from "../actionsList";
+import axios from "axios";
+
+export function setChats(value) {
+    return {
+        type: SET_CHATS,
+        value: value,
+    }
+}
+
+export function asyncSetChats() {
+    return async function (dispatch) {
+        let chats = await axios.get("http://localhost:5050/getChat", {
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        dispatch(setChats(chats));
+    }
+}
