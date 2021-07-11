563285ff4e746d4807acda4536c662c5ec8f4d97
G-Prokhorov <grisha.prokhorov03@gmail.com>
2021-07-11 12:23:50 +0300

    fix height img

--- src/components/main/message/message.js
+++ src/components/main/message/message.js
@@ -4,9 +4,10 @@ import "./styleMessage.css";
 export default function Message(props) {
     return <div>
         {props.img ?
-            <img className={props.float === "left" ? "messageLeft message messageImg" : "messageRight message messageImg"} src={props.text} alt="image" />
+            <img height={400} className={props.float === "left" ? "messageLeft message messageImg" : "messageRight message messageImg"}
+                src={props.text} alt="image"/>
             :
-            <p className={props.float === "left" ? "messageLeft message" : "messageRight message" }>
+            <p className={props.float === "left" ? "messageLeft message" : "messageRight message"}>
                 {props.text}
             </p>
         }
